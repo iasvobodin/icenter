a474b65997ab8f1ac1c5f46a670b94c2e525691e
iasvobodin <iasvobodin@gmail.com>
2021-02-02 09:46:17 +0500

    auth

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/User">User</router-link> |
+    <router-link to="/Admin">Admin</router-link>
   </div>
   <router-view />
 </template>
