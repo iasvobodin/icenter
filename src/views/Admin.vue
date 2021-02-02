323c864ed0a93329a3edb9dbfc7cec91ccdd656f
iasvobodin <iasvobodin@gmail.com>
2021-02-02 15:28:51 +0500

    ttest

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,42 +1,63 @@
 <template>
   <h1>Admin Page With Auth</h1>
-  <h2 v-if="payload">{{ payload }}</h2>
+  <!-- <Suspense>
+    <h2>{{ payload }}</h2>
+  </Suspense> -->
+  <h3>{{ payload.userDetails }}</h3>
+  <h2 v-for="item in state" :key="item.id">{{ item.body }}</h2>
 </template>
 
 <script>
 /* eslint-disable no-unused-vars */
 
-import { ref } from "vue";
-import { defineComponent } from "vue";
+import { ref, onMounted } from "vue";
+// import { defineComponent } from "vue";
 
-async function getUser() {
-  const response = await fetch("/api/testfunc");
-  const payload = await response.json();
-  const { clientPrincipal } = payload;
-  return clientPrincipal;
-}
+// async function getUser() {
+//   const response = await fetch("/api/testfunc");
+//   const payload = await response.json();
+//   const { clientPrincipal } = payload;
+//   return clientPrincipal;
+// }
 
-console.log(getUser(), "fromapi");
+// console.log(getUser(), "fromapi");
 
-async function getUserInfo() {
-  const response = await fetch("/.auth/me");
-  const payload = await response.json();
-  const { clientPrincipal } = payload;
-  return clientPrincipal;
-}
-console.log(getUserInfo(), "promises");
+// async function getUserInfo() {
+//   const response = await fetch("/.auth/me");
+//   const payload = await response.json();
+//   const { clientPrincipal } = payload;
+//   return clientPrincipal;
+// }
+// console.log(getUserInfo(), "promises");
 
 export default {
   setup() {
     const payload = ref(null);
-    async function getUserInfo() {
-      const response = await fetch("/.auth/me");
-      payload.value = await response.json();
-      // const { clientPrincipal } = payload;
-      // return clientPrincipal;
-    }
+    const state = ref(null);
+    fetch("/.auth/me")
+      .then(response => response.json())
+      .then(data => {
+        payload.value = data;
+      });
+    // onMounted(async () => {
+    //   const response = await fetch("/.auth/me");
+    //   payload.value = await response.json();
+    // });
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(json => {
+        state.value = json;
+        console.log(json);
+      });
+    // async function getUserInfo() {
+    //   const response = await fetch("/.auth/me");
+    //   payload.value = await response.json();
+    //   // const { clientPrincipal } = payload;
+    //   // return clientPrincipal;
+    // }
     // const users = await getUserInfo();
-    return { payload };
+    return { payload, state };
   }
 };
 </script>
