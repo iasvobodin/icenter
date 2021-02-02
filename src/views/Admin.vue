ade249aad0e1599d96d1a0fa12c2d02e40b8530d
iasvobodin <iasvobodin@gmail.com>
2021-02-02 14:42:25 +0500

    aaa

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,27 +1,32 @@
 <template>
   <h1>Admin Page With Auth</h1>
-  <!-- <h2>{{ user.userDetails }}</h2> -->
+  <h2 v-if="payload">{{ payload }}</h2>
 </template>
 
 <script>
+/* eslint-disable no-unused-vars */
+
+import { ref } from "vue";
+import { defineComponent } from "vue";
 async function getUserInfo() {
   const response = await fetch("/.auth/me");
   const payload = await response.json();
   const { clientPrincipal } = payload;
   return clientPrincipal;
 }
+console.log(getUserInfo(), "promises");
 
-console.log(getUserInfo());
 export default {
   setup() {
-    // async function getUserInfo() {
-    //   const response = await fetch("/.auth/me");
-    //   const payload = await response.json();
-    //   const { clientPrincipal } = payload;
-    //   return clientPrincipal;
-    // }
-    // const user = getUserInfo();
-    return {};
+    const payload = ref(null);
+    async function getUserInfo() {
+      const response = await fetch("/.auth/me");
+      payload.value = await response.json();
+      // const { clientPrincipal } = payload;
+      // return clientPrincipal;
+    }
+    // const users = await getUserInfo();
+    return { payload };
   }
 };
 </script>
