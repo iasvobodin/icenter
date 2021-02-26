b8ef76995a7096dfb1f6993e1f5baa37afb9d488
iasvobodin <iasvobodin@gmail.com>
2021-02-26 09:45:49 +0500

    ll

---
+++ src/views/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+export default {
+  setup() {
+    const payload = ref(null);
+    const state = ref(null);
+    const router = useRouter();
+    console.log(router, "this");
+
+    fetch("/.auth/login/aad").then(() =>
+      router.push(`/`)
+    );
+
+    // fetch("https://jsonplaceholder.typicode.com/users/1")
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     router.push(`/admin/${json.id}`);
+    //     // state.value = json;
+    //     // console.log(json);
+    //   });
+
+    return { payload, state };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
