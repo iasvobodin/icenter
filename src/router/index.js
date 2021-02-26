ca5bb1713346c7ff7d9c12b716d0778403b3b03e
iasvobodin <iasvobodin@gmail.com>
2021-02-26 09:49:38 +0500

    dd

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,10 @@ const routes = [
     name: "Home",
     component: Home
   },
-  {
-    path: "/.auth/login/aad",
-    name: "login"
-  },
+  // {
+  //   path: "/.auth/login/aad",
+  //   name: "login"
+  // },
   {
     path: "/User",
     name: "User",
@@ -39,6 +39,16 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: function() {
       return import(/* webpackChunkName: "about" */ "../views/Admin.vue");
+    }
+  },
+  {
+    path: "/login",
+    name: "login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function() {
+      return import(/* webpackChunkName: "about" */ "../views/login.vue");
     }
   }
 ];
