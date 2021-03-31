1cf16ecccbe054844de71cad215b98166ebd4f67
iasvobodin <iasvobodin@gmail.com>
2021-03-31 23:58:47 +0500

    auth

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="about">
     <h1>User page</h1>
+    <h2 v-if="user">{{ user }}</h2>
   </div>
 </template>
 
- 
  <script>
 export default {
+  data() {
+    return {
+      user: null,
+    };
+  },
   mounted() {
     async function getUserInfo() {
       const response = await fetch("/.auth/me");
       const payload = await response.json();
       const { clientPrincipal } = payload;
+      this.user = clientPrincipal.userDetails;
       return clientPrincipal;
     }
 
