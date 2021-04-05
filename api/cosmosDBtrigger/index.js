9d87ddd778ec74a6ba0abd399db7c0d4aebd0d35
iasvobodin <iasvobodin@gmail.com>
2021-04-05 11:15:29 +0500

    delete cosmosDBtriger

--- api/cosmosDBtrigger/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function(context//, documents, testOut
-    ) {
-  context.log("!!!!!!!!!!!!!!rr555555555555r!!!!!!!!!!!!!!!!",context.bindings);
-
-  return context.bindings.documents[0]
-  
-  //testOut = documents[0].id
-//   context.done();
-};
