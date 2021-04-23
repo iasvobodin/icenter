59ffdd1c94510a3d51e2645b87b4fad076ddfa19
iasvobodin <iasvobodin@gmail.com>
2021-04-23 16:23:23 +0500

    1111111

--- src/views/errors/index.vue
+++ src/views/errors/index.vue
@@ -1,6 +1,24 @@
 <template>
+    <div
+      v-for="(value, key, index) in errors"
+      :key="index"
+      class="errors__card"
+      @click="chosseError(value.id)"
+    >
+        <div
+          v-for="(val, key, index) in val.info"
+          :key="index"
+          class="cabinet__info__item"
+        >
+          <h3>{{ key }}:</h3>
+          <p>
+            {{ val }}
+          </p>
+        </div>
+    </div>
+    
   <div>
-      <h1 v-if="errors"> !!!!!!!!!!!!!!!!!!!!!!!{{errors}}</h1>
+      <h1 @click="$router.push(`/errors/${errors[0].id}`)" v-if="errors">{{errors}}</h1>
   </div>
 </template>
 
