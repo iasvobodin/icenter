87b577cb37ec3d19daf48cb13bad9f37d3d368f6
iasvobodin <iasvobodin@gmail.com>
2021-04-26 10:08:36 +0500

    err

--- src/views/errors/index.vue
+++ src/views/errors/index.vue
@@ -1,24 +1,23 @@
 <template>
+<div class="errors__holder" >
     <div
       v-for="(value, key, index) in errors"
       :key="index"
       class="errors__card"
       @click="chosseError(value.id)"
     >
-        <div
-          v-for="(val, key, index) in val.info"
-          :key="index"
-          class="cabinet__info__item"
-        >
-          <h3>{{ key }}:</h3>
-          <p>
-            {{ val }}
-          </p>
-        </div>
+              <div  v-for="(val, key, index) in value.info"
+          :key="index" class="error__item">
+        <h3 class="error__item__title">{{ key }}:</h3>
+        <p class="error__item__desc">
+         {{ val }}
+        </p>
+      </div>
     </div>
     
   <div>
-      <h1 @click="$router.push(`/errors/${errors[0].id}`)" v-if="errors">{{errors}}</h1>
+      <!-- <h1 @click="$router.push(`/errors/${errors[0].id}`)" v-if="errors">{{errors}}</h1> -->
+  </div>
   </div>
 </template>
 
@@ -30,6 +29,9 @@ export default {
         }
     },
     methods: {
+          chosseError(e) {
+      this.$router.push(`/errors/${e}`);
+    },
       async  getErrors() {
        const resErrors =   await fetch(`/api/errors?status=${"open"}`)
        this.errors = await resErrors.json()
@@ -41,4 +43,40 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+.errors__holder {
+  display: grid;
+  /* width: 90%;
+  margin: auto; */
+  grid-template-columns: repeat(auto-fit, minmax(max(25vw, 250px), 1fr));
+  column-gap: 2vh;
+  row-gap: 2vh;
+}
+.errors__card {
+  border: 1px solid orange;
+  border-radius: 4px;
+  padding: 5px;
+  cursor: pointer;
+}
+.errors__card:hover {
+  border: 1px solid black;
+  background: rgba(245, 254, 255, 0.356);
+}
+.error__item {
+  border-bottom: 1px solid black;
+  padding: 2px;
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+}
+.error__item__title {
+  justify-self: start;
+  align-self: center;
+  text-align: start;
+  margin: 0;
+}
+.error__item__desc {
+  justify-self: end;
+  text-align: end;
+  align-self: center;
+  margin: 0;
+}</style>
