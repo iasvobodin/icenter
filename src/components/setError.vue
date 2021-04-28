fa9c49c1526060dbb4f4a53d7a5569197216b237
iasvobodin <iasvobodin@gmail.com>
2021-04-28 10:06:20 +0500

    test error

--- src/components/setError.vue
+++ src/components/setError.vue
@@ -2,52 +2,28 @@
   <!-- <div>SET ERROR</div> -->
   <div v-if="$store.state.template">
     <form @submit.prevent="postError">
-      <div v-if="$store.state.user.info.userRoles.includes('admin')">
-        <select class="change__status" v-model="role">
+      <div
+        class="error__item"
+        v-if="$store.state.user.info.userRoles.includes('admin')"
+      >
+      <h4 class="error__item__title">Выберете роль</h4>  
+        <select class="change__status error__item__desc" v-model="role">
           <option selected value="fitter">Сборщик</option>
           <option value="testEngeneer">Тестировщик</option>
         </select>
         <!-- <button>Fitter</button><button>TetsEngeneer</button> -->
       </div>
       <!-- eslint-disable-next-line vue/no-use-v-if-with-v-for -->
-      <div v-if="role === 'fitter'"
-        class="error__field"
-        v-for="(value, key, index) in $store.state.template.error.body.Открыто"
+      <div  v-if="role === 'fitter'"
+        :class="{ error__item__desc: key==='Описание' }"
+          class="error__item"
+        v-for="(value, key, index) in $store.state.template.error.body2.Открыто"
         :key="index"
       >
-        <p>{{ key }}</p>
-        <br />
-        <select
-          required
-          v-if="typeof value === 'object'"
-          v-model="errorBody[key]"
-        >
-          <option v-for="(opt, index) in value" :key="index">
-            {{ opt }}
-          </option>
-        </select>
-        <textarea
-          v-else
-          required
-          v-model="errorBody[key]"
-          cols="50"
-          rows="5"
-        ></textarea>
-      </div>
-      <div v-else>
-        <div
-          class="error__field"
-          v-for="(value, key, index) in $store.state.template.error
-            .testEngeneer"
-          :key="index"
-        >
-          <p>{{ key }}</p>
-          <br />
-          <select
+        <h4 :class="{ error__item__vertical__title: key==='Описание' }"  class="error__item__title">{{ key }}</h4>
+       <select class="error__item__desc"
             v-model="errorBody[key]"
             v-if="Object.values(value)[0] === 'select'"
-            name=""
-            id=""
           >
             <option v-for="(v, i) in Object.values(value)[1]" :key="i">
               {{ v }}
@@ -66,14 +42,64 @@
           <textarea
             v-model="errorBody[key]"
             v-if="Object.values(value)[0] === 'textarea'"
-            name=""
-            id=""
+            cols="30"
+            rows="5"
+          ></textarea>
+      </div>
+      <div v-else>
+        <div
+          :class="{ error__item__desc: key==='Описание' }"
+          class="error__item"
+          v-for="(value, key, index) in $store.state.template.error.testEngeneer"
+          :key="index"
+        >
+          <h4 :class="{ error__item__vertical__title: key==='Описание' }"  class="error__item__title">{{ key }}</h4>
+          <select class="error__item__desc"
+            v-model="errorBody[key]"
+            v-if="Object.values(value)[0] === 'select'"
+          >
+            <option v-for="(v, i) in Object.values(value)[1]" :key="i">
+              {{ v }}
+            </option>
+          </select>
+          <input
+            v-model="errorBody[key]"
+            v-if="Object.values(value)[0] === 'checkbox'"
+            type="checkbox"
+          />
+          <input
+            v-model="errorBody[key]"
+            v-if="Object.values(value)[0] === 'number'"
+            type="number"
+          />
+          <textarea
+            v-model="errorBody[key]"
+            v-if="Object.values(value)[0] === 'textarea'"
             cols="30"
             rows="5"
           ></textarea>
         </div>
       </div>
-
+      <div v-if="errorBody['Ошибку допустил']&&role === 'testEngeneer'">
+        <div
+          class="error__field"
+          v-if="errorBody['Ошибку допустил'] === 'Инженер-проектировщик'"
+        >
+          <select name="" id="">
+            <option value="">инж1</option>
+            <option value="">инж2</option>
+          </select>
+        </div>
+        <div
+          class="error__field"
+          v-if="errorBody['Ошибку допустил'] === 'Сборщик'"
+        >
+          <select name="" id="">
+            <option value="">сб1</option>
+            <option value="">сб2</option>
+          </select>
+        </div>
+      </div>
       <!-- <input type="file" /> -->
       <input class="add__button" type="submit" value="добавить в базу" />
     </form>
@@ -174,6 +200,31 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.cabinet__info__item {
+  border-bottom: 1px solid black;
+  padding: 5px;
+  /* width: 100%; */
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+}
+.error__item {
+  border-bottom: 1px solid black;
+  padding: 2px;
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+}
+.error__item__title {
+  justify-self: start;
+  align-self: center;
+  text-align: start;
+  margin: 0;
+}
+.error__item__desc {
+  justify-self: end;
+  text-align: end;
+  align-self: center;
+  margin: 0;
+}
 .error__field {
   margin: 10px;
   box-sizing: border-box;
@@ -181,5 +232,16 @@ export default {
 form {
   width: min(95vw, 400px);
   margin: auto;
+}
+.error__item__desc {
+  display: block;
+  justify-self: end;
+  text-align: end;
+  align-self: center;
+  margin: 0;
+}
+.error__item__vertical__title {
+  text-align: center;
+  margin: 5px;
 }
 </style>
