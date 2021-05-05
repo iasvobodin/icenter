bb253e9e4eed604b6d832148b70416d1b680fe73
iasvobodin <iasvobodin@gmail.com>
2021-05-05 15:06:53 +0500

    start typing

---
+++ src/vuex.d.ts
@@ -0,0 +1,15 @@
+import { ComponentCustomProperties } from 'vue'
+import { Store } from 'vuex'
+
+declare module '@vue/runtime-core' {
+  // declare your own store states
+  interface State {
+    count: number
+    projectInfo: {} as IPrInfo
+  }
+
+  // provide typings for `this.$store`
+  interface ComponentCustomProperties {
+    $store: Store<State>
+  }
+}
