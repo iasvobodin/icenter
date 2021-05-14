68bb330c9e36931b4684c1247b8062115e557ef1
iasvobodin <iasvobodin@gmail.com>
2021-05-14 16:15:20 +0500

    qr scaner

--- src/views/cabinets/index.vue
+++ src/views/cabinets/index.vue
@@ -1,12 +1,16 @@
 <template>
 <h1>{{qr}}</h1>
-  <video id="qr-video" playsinline="" disablepictureinpicture="" style="transform: scaleX(-1);"></video>
-  <button @click="scanner.start()">SCAN</button>
+  <!-- <video id="qr-video" playsinline="" disablepictureinpicture="" style="transform: scaleX(-1);"></video>
+  <button @click="scanner.start()">SCAN</button> -->
+  <!-- <div id="reader" style="width: 500px"></div> -->
+  <QRcodeScanner/>
 </template>
-
 <script>
-import QrScanner from 'qr-scanner';
+import QRcodeScanner from '@/components/QRcodeScanner.vue'
 export default {
+    components: {
+    QRcodeScanner
+  },
   data() {
     return {
       qr: '',
@@ -14,10 +18,19 @@ export default {
     }
   },
   mounted () {
-    const video = document.getElementById('qr-video');
-    // QrScanner.hasCamera().then();
-    this.scanner = new QrScanner(video, result => this.qr = result);
-   
+    function onScanSuccess(qrCodeMessage) {
+      console.log(qrCodeMessage);
+	// handle on success condition with the decoded message
+}
+    // const video = document.getElementById('qr-video');
+    // // QrScanner.hasCamera().then();
+    // this.scanner = new QrScanner(video, result => this.qr = result);
+  //  const html5QrcodeScanner = new Html5QrcodeScanner(
+	// "reader", { fps: 10, qrbox: 250 });
+
+  // console.log(globalThis);
+
+// html5QrcodeScanner.render(onScanSuccess);
   },
 };
 </script>
