6e2b83027dde33d7151f966ed36fe092f5b7103c
iasvobodin <iasvobodin@gmail.com>
2021-08-05 13:35:44 +0500

    9090

--- src/router/index.js
+++ src/router/index.js
@@ -104,8 +104,8 @@ const router = createRouter({
 router.beforeEach((to, from) => {
   const user = JSON.parse(window.localStorage.getItem("user")) 
   if (to.path === '/login')  return true
-  // if (!user) return '/login'
-  if (!user.info.userRoles.includes('icenter')) {
+  if (!user) return '/login'
+  if (!user?.info.userRoles.includes('icenter')) {
     return '/login'
   } else {
     return true
