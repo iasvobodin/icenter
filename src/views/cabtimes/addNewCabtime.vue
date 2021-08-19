9a6106518b380ff5f56462a95b67afcb39476e86
iasvobodin <iasvobodin@gmail.com>
2021-08-19 13:12:07 +0500

    cabtime

--- src/views/cabtimes/addNewCabtime.vue
+++ src/views/cabtimes/addNewCabtime.vue
@@ -9,13 +9,12 @@
     </div>
 <section v-if="cabinet">
         <div v-for="(t,i) in $store.state.template.CabTimeGroup">
-            <!-- <h3>{{t}} </h3> -->
             <table>
                 <colgroup>
                     <col span="1" style="width: 5%;">
                     <col span="1" style="width: 70%;">
-                    <col span="1" style="width: 15%;">
-                    <col span="1" style="width: 5%;">
+                    <col span="1" style="width: 10%;">
+                    <col span="1" style="width: 10%;">
                     <col span="1" style="width: 5%;">
                 </colgroup>
                 <tbody>
@@ -24,20 +23,34 @@
                         <th>{{t.name}}</th>
                         <th>Кол-во</th>
                         <th>Норма</th>
-                        <th><div v-if="cabtimetype">{{cabtimetype[i].summ}}</div></th>
+                        <th>{{cabtimetype&&cabtimetype[i].summ}}</th>
                         <!-- <th>Index</th> -->
                     </tr>
                     <tr v-for="(value, index) in  groupBy(t.name)" :key="index">
-                        <td>{{ value._id }}</td>
-                        <td class="cabtime__name">{{ value.name }}</td>
-                        <td class="tg-0lax"><input class="cabtime__input" type="number"
-                                @input="inputData($event, value._id)"></td>
-                        <td class="tg-0lax">{{ value._const }}</td>
-                        <td class="tg-0lax">
-                            <div v-if="tt" >{{value.result}}</div>
+                        <td>
+                            {{ value._id }}
+
                         </td>
-                        <!-- <td>{{`${i+1}.${index+1}`}}</td> -->
+                        <td v-if="value.new" class="cabtime__name">
+                            <textarea rows="1"></textarea>
+                        </td>
+                        <td v-else class="cabtime__name">
+                            {{ value.name }}
+                        </td>
+                        <td>
+                            <input class="cabtime__input" type="number" :value="value.value" @input="inputData($event, value._id, 'value')">
+                        </td>
+                        <td v-if="value.new">
+                            <input class="cabtime__input" type="number" @input="inputData($event, value._id, '_const')">
+                        </td>
+                        <td v-else>
+                            {{ value._const }}
+                        </td>
+                        <td>
+                            {{tt&&value.result}}
+                        </td>
                     </tr>
+                    <div class="add__row" @click="addNewRow(t.name)"> + </div>
                 </tbody>
             </table>
         </div>
@@ -77,25 +90,81 @@ export default {
             fetchProject: null,
             projectInformation: null,
             projectData: null,
-            cabinet:'',
+            cabinet: '',
             cabtimetype: null,
         })
-        const inputData = ($event, key) => {
+        const inputData = ($event, key, val) => {
             !state.tt && (state.tt = store.state.template.CabTimeV2);
             !state.cabtimetype && (state.cabtimetype = store.state.template.CabTimeGroup);
+            let arr, coef;
+            switch (key) {
+                case '1.3':
+                    arr = ['2.3', '2.4']
+                    coef = 1
+                    break;
+                case '1.4':
+                    arr = ['2.1', '2.2']
+                    coef = 1
+                    break;
+                case '1.5':
+                    arr = ['2.5', '2.6']
+                    coef = 1
+                    break;
+                case '3.3':
+                    arr = ['3.4']
+                    coef = 1
+                    break;
+                case '4.4':
+                    arr = ['4.5', '4.6']
+                    coef = 1
+                    break;
+                case '4.8':
+                    arr = ['4.9', '4.10']
+                    coef = 2
+                    break;
+                case '5.1':
+                    arr = ['5.2', '5.3']
+                    coef = 2
+                    break;
+                case '5.4':
+                    arr = ['5.5', '5.6']
+                    coef = 2
+                    break;
+                case '5.9':
+                    arr = ['5.10', '5.11']
+                    coef = 2
+                    break;
+                default:
+                    break;
+            }
             state.tt.map(e => {
-                if (e._id === key) {
-                    e.value = $event.target.value
-                    e.result = $event.target.value * e._const
+                if (arr) {
+                    arr.forEach(el => {
+                        if (e._id === el) {
+                            e[val] = $event.target.value * coef
+                            e.result = Math.round(e.value * e._const)
+                        }
+                    });
+
+                    // if (e._id === '2.4') {
+                    //     e[val] = $event.target.value
+                    //     e.result = Math.round(e.value * e._const)
+                    // }
                 }
+                if (e._id === key) {
+                    e[val] = $event.target.value
+                    e.result = Math.round(e.value * e._const)
+                }
+
             })
             state.cabtimetype.map(e => {
-                return e.summ = state.tt.filter(f => f._type === e.name).reduce((acc, m) => {return m.result ? acc += +m.result: acc }, 0)
+                return e.summ = state.tt.filter(f => f._type === e.name).reduce((acc, m) => {
+                    return m.result ? acc += +m.result : acc
+                }, 0)
             })
-                // debugger
         }
         // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-        const summByType = computed(()=> state.tt?state.tt.push(store.state.template.CabTime.reduce((acc, el) => acc.add(el._type), new Set())) : 0)
+        // const summByType = computed(() => state.tt ? state.tt.push(store.state.template.CabTime.reduce((acc, el) => acc.add(el._type), new Set())) : 0)
         //  t =>  state.tt&&state.tt.reduce((acc,f) => f._type === t&&(acc +=f.result),0);
         const result = (a, b) => Math.ceil(a * b)
         const getType = computed(() => store.state.template && store.state.template.CabTime.reduce((acc, el) => acc.add(el._type), new Set()))
@@ -117,43 +186,60 @@ export default {
             }
         }
         fetchProjectList()
-           const chooseCabinet = (e) => {
-      state.cabinet = e.split('   ')[0];
-      store.commit("SETcabinetInfo", e);
-    //   this.woState = true;
-    }
+        const chooseCabinet = (e) => {
+            state.cabinet = e.split('   ')[0];
+            store.commit("SETcabinetInfo", e);
+            //   this.woState = true;
+        }
         const choose = ($event) => {
-                if (!$event) {
-                    state.projectInformation = false;
-                    return;
-                }
-                state.projectInformation = state.fetchProject.filter(
-                    (e) => e.id === $event
-                )[0];
-                //   console.log(this.projectInformation, "this.projectInformation");
-                store.commit("SETprojectInfo", state.projectInformation);
+            if (!$event) {
+                state.projectInformation = false;
+                return;
             }
+            state.projectInformation = state.fetchProject.filter(
+                (e) => e.id === $event
+            )[0];
+            //   console.log(this.projectInformation, "this.projectInformation");
+            store.commit("SETprojectInfo", state.projectInformation);
+        }
 
-            const postCabTime = async () => {
-                const cabTime = {
-                    "id": `cabtime__${Date.now()}`,
-                    "info": {
-                        Проект: store.state.projectInfo['project number'],
-                        Шкаф: store.state.projectInfo['cab name'],
-                        wo: store.state.projectInfo.wo.toString(),
-                    },
-                    "type": "cabtime",
-                    "body": state.tt
-                }
-                await fetch('/api/POST_error', {
-                    method: 'POST', // или 'PUT'
-                    body: JSON.stringify({
-                        ...cabTime
-                    }),
-                })
-                // console.log(route.params.cabtimeId);
+        const postCabTime = async () => {
+            const cabTime = {
+                "id": `cabtime__${Date.now()}`,
+                "info": {
+                    Проект: store.state.projectInfo['project number'],
+                    Шкаф: store.state.projectInfo['cab name'],
+                    wo: store.state.projectInfo.wo.toString(),
+                },
+                "type": "cabtime",
+                "body": state.tt
             }
+            await fetch('/api/POST_error', {
+                method: 'POST', // или 'PUT'
+                body: JSON.stringify({
+                    ...cabTime
+                }),
+            })
+            // console.log(route.params.cabtimeId);
+        }
+        const addNewRow = (e) => {
+            const ff = state.tt.filter(g => g._type === e)
+            const id = ff[ff.length - 1]._id.split('.')
+            const dd = [id[0], +id[1] + 1].join('.')
+
+            state.tt.push({
+                new: true,
+                "_id": dd,
+                "name": "",
+                "_const": "",
+                "_type": e,
+                "_field": "",
+                "value": ""
+            }, )
+            console.log(e)
+        };
         return {
+            addNewRow,
             getType,
             groupBy,
             postCabTime,
@@ -171,6 +257,20 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.add__row{
+    border: 1px solid orange;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    margin-top: 5px;
+    font-size: 30px;
+    line-height: 1;
+}
+.add__row:hover{
+    color: white;
+    background-color: orange;
+}
 .cabtime__name{
     text-align: start;
 }
