0c62c63c396591ffac08f668c385fe7afeaf363f
iasvobodin <iasvobodin@gmail.com>
2021-08-25 14:38:56 +0500

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
 self.addEventListener('install', event => {
+  self.skipWaiting();
     console.log('Service worker installing...');
     // Add a call to skipWaiting here
   });
@@ -6,3 +7,48 @@ self.addEventListener('install', event => {
   self.addEventListener('activate', event => {
     console.log('Service worker activating...');
   });
+const staticCacheName = 'test-cache-v1'
+
+  // self.addEventListener('fetch', event => {
+  //   if (event.request.url ==='http://localhost:8080/api/projects?status=open') {
+  //         // TEST RESPOND
+  //   // event.respondWith(
+  //   //  (async ()=> fetch(event.request))()
+  //   // )
+  //   // const preFetct = async () => {
+  //   //   const cache = await caches.open(staticCacheName);
+  //   //   const data = await fetch(event.request)
+  //   //   const cc = await cache.put(event.request.url, data.clone());
+  //   //   console.log(cc);
+  //   // };
+  //   // event.waitUntil(preFetct());
+
+
+
+
+
+
+
+
+
+  //       event.respondWith(
+  //         //CHECK OUR CACHE
+  //         caches.match(event.request)
+  //         .then(response => {
+  //           if (response) {
+  //             console.log('Found ', event.request.url, ' in cache');
+  //             return response;
+  //           }
+  //           console.log('Network request for ', event.request.url);
+  //           return fetch(event.request)
+  //           .then(response => {
+  //               return caches.open(staticCacheName).then(cache => {
+  //                 cache.put(event.request.url, response.clone());
+  //                 return response;
+  //               });
+  //             });
+      
+  //         })
+  //   )
+  // }
+  // });
--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 <script>
 import loader from '@/components/loader.vue'
 import appHeader from "@/components/header.vue";
+import * as signalR from '@microsoft/signalr'
 // import { onBeforeRouteLeave, onBeforeRouteUpdate } from "vue-router";
 // import { ref } from "vue";
 export default {
@@ -27,41 +28,6 @@ match(){
   return true
 }
   },
-  //   beforeRouteEnter (to, from, next) {
-  //     // return false
-  //     console.log('here');
-  //     next({name: 'login'})
-  // // next(vm => {
-  // //   console.log(vm, 'vm');
-  // //   // access to component public instance via `vm`
-  // // })
-  // },
-  // setup() {
-  //   // same as beforeRouteLeave option with no access to `this`
-  //   onBeforeRouteLeave((to, from) => {
-  //     const answer = window.confirm(
-  //       "Do you really want to leave? you have unsaved changes!"
-  //     );
-  //     // cancel the navigation and stay on the same page
-  //     if (!answer) return false;
-  //   });
-
-  //   const userData = ref();
-
-  //   // same as beforeRouteUpdate option with no access to `this`
-  //   onBeforeRouteUpdate(async (to, from) => {
-  //     // only fetch the user if the id changed as maybe only the query or the hash changed
-  //     if (to.params.id !== from.params.id) {
-  //       userData.value = await fetchUser(to.params.id);
-  //     }
-  //   });
-  // },
-  // beforeRouteEnter(to, from) {
-  //   console.log("beforeRouteEnter");
-  //   // called before the route that renders this component is confirmed.
-  //   // does NOT have access to `this` component instance,
-  //   // because it has not been created yet when this guard is called!
-  // },
   mounted() {
     this.$store.state.user.body?.bg&&document.documentElement.style.setProperty('--bg', `${this.$store.state.user.body.bg}`);
     // this.$store.state.user.body?.customCursor&&document.documentElement.style.setProperty('--cursor', `url('/img/unicorn.png')`);
@@ -88,26 +54,68 @@ match(){
     console.log(formatDate(dd))
   },
   created() {
-   
 
-    fetch('/api/negotiate');
-      const user = window.localStorage.getItem("user")
-      // console.log(user, 'user');
-  // if (user) {
-  //     this.$store.commit('setUserAuth', user)
-  //   } else{
-  //     console.log('not user');
-  //     this.$store.dispatch('GET_auth')
-  //   }
-    // !this.$store.state.user.info && this.$store.dispatch('GET_auth')
+    const connect = async () => {
+      await await fetch('/api/negotiate');
+
+      const connection = new signalR.HubConnectionBuilder()
+        .withUrl('/api')
+        .build();
+
+      connection.onclose(() => {
+        console.log('SignalR connection disconnected');
+        setTimeout(() => connect(), 2000);
+      });
+
+      connection.on('updated', updatedStock => {
+
+        //CHECK AND PUSH NOTIFICATION
+console.log('try to push');
+        if (!('Notification' in window)) {
+          console.log('This browser does not support notifications!');
+          return;
+        }
+
+        Notification.requestPermission(status => {
+          console.log('Notification permission status:', status);
+        });
+        if (Notification.permission == 'granted' && 'serviceWorker' in navigator) {
+          navigator.serviceWorker.getRegistration().then(reg => {
+            console.log(reg);
+            const options = {
+              body: 'is updated',
+              vibrate: [100, 50, 100],
+              data: {
+                dateOfArrival: Date.now(),
+                primaryKey: 1
+              },
+            };
+            reg.showNotification(`Project ${updatedStock.id}`, options);
+          });
+        }
+        // console.log(updatedStock);
+        // alert(`${updatedStock.id} обновлена`)
+      });
+
+      connection.start().then(() => {
+        console.log("SignalR connection established");
+      });
+    };
+
+    connect();
+
+
+    this.$store.state.user.body ?.customCursor && document.documentElement.style.setProperty('--cursor__p', `url('/img/unicorn2.png')`)
+    this.$store.state.user.body ?.customCursor && document.documentElement.style.setProperty('--cursor', `url('/img/unicorn.png')`)
+    this.$store.state.user.body ?.bg && document.documentElement.style.setProperty('--bg', `${this.$store.state.user.body.bg}`)
+    
     const getTT = async () => {
       await this.$store.dispatch('GET_template')
-      // console.log(this.$store.state.user.body&&this.$store.state.user.body.customCursor, 'cursor');
-      this.$store.state.user.body?.customCursor&&document.documentElement.style.setProperty('--cursor__p', `url('/img/unicorn2.png')`);
-      this.$store.state.user.body?.customCursor&&document.documentElement.style.setProperty('--cursor', `url('/img/unicorn.png')`);
-      this.$store.state.user.body?.bg&&document.documentElement.style.setProperty('--bg', `${this.$store.state.user.body.bg}`);
     }
     getTT()
+
+
+
   },
 }
 </script>
