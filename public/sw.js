993e7322db20ea04dd1086dfe6e73266530e1c83
iasvobodin <iasvobodin@gmail.com>
2021-09-01 16:27:32 +0500

    idb

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,20 @@
 
-if (typeof idb === "undefined") {
   self.importScripts('../src/hooks/idb.js');
-}
 
 function createDB() {
   if (!('indexedDB' in self)) {
     console.log('This browser doesn\'t support IndexedDB');
     return;
   }
-  const dbPromise = idb.open('products', 1, (upgradeDB) => {
-    if (!upgradeDb.objectStoreNames.contains('beverages')) {
-     const store = upgradeDb.createObjectStore('beverages', {
-      keyPath: 'id'
-    });
-    }
-    var store = upgradeDB.createObjectStore('beverages', {
+  const dbPromise = idb.open('products', 1, upgradeDB => {
+    if (!upgradeDB.objectStoreNames.contains('beverages')) {
+     const store = upgradeDB.createObjectStore('beverages', {
       keyPath: 'id'
     });
     store.put({id: 123, name: 'coke', price: 10.99, quantity: 200});
     store.put({id: 321, name: 'pepsi', price: 8.99, quantity: 100});
     store.put({id: 222, name: 'water', price: 11.99, quantity: 300});
+    }
   });
 }
 
