18a8360d22e32d70eff97e899acfd077a2d0d20a
iasvobodin <iasvobodin@gmail.com>
2021-10-08 12:24:12 +0500

    new auth test mode

--- src/router/index.js
+++ src/router/index.js
@@ -127,48 +127,138 @@ router.beforeEach((to, from) => {
   if (to.path === '/login')  return true
 })
 
-const CHECK_auth = async () =>{
-try {
-  const responseUserAuth = await fetch("/.auth/me");
-  if (responseUserAuth.ok) {
-      const user = await responseUserAuth.json();
-      return user
-      // window.localStorage.setItem("user", JSON.stringify(userAuth));
+
+const createName = (clientPrincipal) => {
+  if (clientPrincipal.identityProvider === 'aad') {
+    const splitName = clientPrincipal.userDetails.split('@')[0].split('.')
+    const name = splitName[0][0].toUpperCase() + '.' + splitName[1][0].toUpperCase() + '.'
+    return name
   }
-  return false
-  
-} catch (error) {
-  console.log(error);
-}
+  return clientPrincipal.userDetails.split('@')[0]
 }
 
+const CHECK_auth = async () => {
+  try {
+    const responseUserAuth = await fetch("/.auth/me");
+    if (responseUserAuth.ok) {
+      const user = await responseUserAuth.json();
+      const {
+        clientPrincipal
+      } = user
+      //CREATE OBJECT FOR SERVER
+      const userRes = {
+        id: clientPrincipal.userId,
+        type: "info",
+        info: clientPrincipal,
+        body: {
+          name: createName(clientPrincipal)
+        },
+      }
+      return userRes
+    }
+    return false
+  } catch (error) {
+    console.log(error, 'CHECK AUTH ERROR');
+  }
+}
+const commitUser = (user) => {
+  window.localStorage.setItem("user", JSON.stringify(user));
+  store.commit("setUserAuth", JSON.stringify(user));
+}
+const CHECK_userDB = async (user) => {
+  try {
+    const registerUserRes = await fetch(`/api/user/${user.info.userId}?getRegisterUser=true`)
+    //   console.log(responseUserAuth,'responseUserAuth');
+    if (registerUserRes.ok) {
+      user = await registerUserRes.json()
+      commitUser(user)
+    } else {
+      try {
+        const options = {
+          method: "POST", // или 'PUT'
+          body: JSON.stringify(user)
+        }
+        await fetch(`/api/user/${user.info.userId}?postRegisterUser=true`, options)
+        commitUser(user)
+      } catch (error) {
+        console.log(error, "error from register user in db");
+      }
+    }
+  } catch (error) {
+    console.log(error, "from check user in db");
+  }
+}
 
 router.beforeEach(async (to, from) => {
-  !store.state.template && await store.dispatch('GET_template')
-  const user = window.localStorage.getItem("user")
+  if (to.path === '/login' || to.path === '/role') return true
 
-// const test = await CHECK_auth()
-console.log(await CHECK_auth(), 'just check user auth from route own');
+    !store.state.template && await store.dispatch('GET_template')
 
-async function getUser() {
-  const response = await fetch('/api/user');
-  const payload = await response.json();
-  const { clientPrincipal } = payload;
-  return clientPrincipal;
-}
-
-console.log(await getUser(), 'just check user auth from route ff');
-
-  if (to.path === '/login' || to.path === '/role')  return true
-  // if (to.path === '/role')  return true
-  if (user) {
-    if (!JSON.parse(user).info.userRoles.includes('icenter')) { //CHECK ROLE
-      return '/role'
+  if (
+    import.meta.env.MODE === 'development') {
+    let user = {
+      "id": "9c0c1980f5904d10b43e552d2b7c4041",
+      "type": "info",
+      "info": {
+        "identityProvider": "aad",
+        "userId": "9c0c1980f5904d10b43e552d2b7c4041",
+        "userDetails": "ivan.svobodin@emerson.com",
+        "userRoles": ["admin", "anonymous", "icenter", "authenticated"]
+      },
+      "body": {
+        "name": "I.S."
+      },
     }
-    store.commit('setUserAuth', user) //ADD USER TO VUEX
-  } else{
-    await  store.dispatch('GET_auth') // TRY TO GET AUTH
+    commitUser(user)
+    return true
+  } else {
+    //CHECK USER GLOBAL
+    let user = window.localStorage.getItem("user")
+
+    if (user) {
+      if (!JSON.parse(user).info.userRoles.includes('icenter')) { //CHECK ROLE
+        return '/role'
+      } else {
+        CHECK_userDB(user)
+        return true
+      }
+    } else {
+      user = await CHECK_auth()
+      if (user) {
+        //CHECK ROLE
+        if (!JSON.parse(user).info.userRoles.includes('icenter')) {
+          return '/role'
+        } else {
+          CHECK_userDB(user)
+          return true
+        }
+      } else {
+        return '/login'
+      }
+    }
   }
+
+  // const test = await CHECK_auth()
+  // console.log(await CHECK_auth(), 'just check user auth from route own');
+
+  // async function getUser() {
+  //   const response = await fetch('/api/user');
+  //   const payload = await response.json();
+  //   const { clientPrincipal } = payload;
+  //   return clientPrincipal;
+  // }
+
+  // console.log(await getUser(), 'just check user auth from route ff');
+
+  // if (to.path === '/role')  return true
+  // if (user) {
+  //   if (!JSON.parse(user).info.userRoles.includes('icenter')) { //CHECK ROLE
+  //     return '/role'
+  //   }
+  //   store.commit('setUserAuth', user) //ADD USER TO VUEX
+  // } else {
+  //   await store.dispatch('GET_auth') // TRY TO GET AUTH
+  // }
 })
 
 export default router;
