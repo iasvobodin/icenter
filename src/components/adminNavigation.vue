e4eb52b5bcf38437da28ffc9652c06520d38aa53
iasvobodin <iasvobodin@gmail.com>
2021-11-24 13:05:17 +0500

    Revert "empty"
    This reverts commit 42a3508c31e84355b0c3bb443db6ef16242f96fc.

--- src/components/adminNavigation.vue
+++ src/components/adminNavigation.vue
@@ -2,7 +2,7 @@
   <div v-if="showMenu" v-click-away="onClickAway" class="navigation">
     <nav>
       <ul>
-        <li> 
+        <li>
           <router-link :to="`/projects`">Проекты</router-link>
         </li>
         <li>
