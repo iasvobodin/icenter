bfa18a175ad3c792bf9eb13e487f6f9edfebe743
iasvobodin <iasvobodin@gmail.com>
2021-11-30 16:13:30 +0500

    ff

--- public/sw.js
+++ public/sw.js
@@ -46,10 +46,6 @@ self.addEventListener('activate', (e) => {
 })
 
 self.addEventListener('fetch', (e) => {
-  //   const modifiedHeaders = new Headers({
-  //     ...e.request.headers,
-  //     'x-ms-client-principal-id': '000000000000000000001'
-  //  });
   e.respondWith(
     caches
       .match(e.request)
