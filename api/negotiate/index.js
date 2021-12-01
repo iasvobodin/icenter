7736185ed279eb0bf8b66e632faeb085a4e1e655
iasvobodin <iasvobodin@gmail.com>
2021-12-01 09:57:14 +0500

    del

--- api/negotiate/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function (context, req, connectionInfo) {
-  context.res.body = connectionInfo
-  // context.res.headers['x-ms-client-principal-id'] = '9c0c1980f5904d10b43e552d2b7c4041'
-}
