da241a55ff141762471e722b8acfbb26cdccdeeb
iasvobodin <iasvobodin@gmail.com>
2021-12-03 16:29:20 +0500

    ddd

--- api/getUserHeader/index.js
+++ api/getUserHeader/index.js
@@ -1,3 +1,24 @@
+const {
+  Client
+} = require("@microsoft/microsoft-graph-client");
+const {
+  TokenCredentialAuthenticationProvider
+} = require("@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials");
+const {
+  DeviceCodeCredential
+} = require("@azure/identity");
+
+const credential = new DeviceCodeCredential(tenantId, clientId, clientSecret);
+const authProvider = new TokenCredentialAuthenticationProvider(credential, {
+  scopes: [scopes]
+});
+
+const client = Client.initWithMiddleware({
+  debugLogging: true,
+  authProvider
+  // Use the authProvider object to create the class.
+});
+
 module.exports = async function (context, req) {
   context.bindings.signalRMessages = [
     {
