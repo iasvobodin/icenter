28b4a4800fe3d9dd791de849580d68d5603295cb
iasvobodin <iasvobodin@gmail.com>
2022-01-18 14:31:57 +0500

    CHECK_AUTH_GLOBAL

--- src/store/index.ts
+++ src/store/index.ts
@@ -109,6 +109,12 @@ export const store = createStore<State>({
     SET_projectList(state, payload) {
       state.projectList = payload
     },
+    SET_USER(state, payload) {
+      window.localStorage.setItem('user', JSON.stringify(payload))
+      // store.commit('setUserAuth', JSON.stringify(payload))
+      state.user = payload as userType
+      // debugger
+    },
   },
   actions: {
     async getCabinetsInfo({ commit }, payload) {
@@ -198,6 +204,128 @@ export const store = createStore<State>({
 
       // }
     },
+
+
+    async CHECK_AUTH_GLOBAL({
+      commit,
+      dispatch,
+      state,
+    }): Promise<string | boolean> {
+      const user: string | null = window.localStorage.getItem('user') // type string
+
+      if (user) {
+        const userParse: userType = JSON.parse(user) // type object local user
+        if (!userParse.info.userRoles.includes('icenter')) {
+          //CHECK ROLE
+          return '/role'
+        } else {
+          //FIRST TIME CHECK
+          Object.keys(state.user).length == 0 &&
+            dispatch('CHECK_AUTH_SERVER', userParse) // LOCALSTORAGE USER
+          return true
+        }
+      } else {
+        const userCheck: false | userType = await dispatch('CHECK_AUTH_AZURE')
+        if (userCheck) {
+          //CHECK ROLE
+          if (!userCheck.info.userRoles.includes('icenter')) {
+            return '/role'
+          } else {
+            await dispatch('CHECK_AUTH_SERVER', userCheck)
+            return true
+          }
+        } else {
+          return '/login'
+        }
+      }
+    },
+
+    async CHECK_AUTH_AZURE(): Promise<false | userType> {
+      const createName = (
+        clientPrincipal: azureAuth['clientPrincipal']
+      ): string => {
+        if (clientPrincipal.identityProvider === 'aad') {
+          const splitName = clientPrincipal.userDetails.split('@')[0].split('.')
+          const name =
+            splitName[0][0].toUpperCase() +
+            '.' +
+            splitName[1][0].toUpperCase() +
+            '.'
+          return name
+        }
+        return clientPrincipal.userDetails.split('@')[0]
+      }
+
+      try {
+        const { request: auth, response } = useFetch<azureAuth>('/.auth/me')
+        await auth()
+        const user = response.value
+
+        const { clientPrincipal } = user!
+        //CREATE OBJECT FOR SERVER
+        if (clientPrincipal) {
+          const userRes = {
+            id: clientPrincipal.userId,
+            type: 'info',
+            info: clientPrincipal,
+            body: {
+              name: createName(clientPrincipal),
+            },
+          }
+          return userRes
+        }
+        return false
+      } catch (error) {
+        console.log(error, 'CHECK AUTH ERROR')
+        return false
+      }
+    },
+    async CHECK_AUTH_SERVER({ commit, dispatch, state }, user: userType | any) {
+      const checkUserInLocal: string | null =
+        window.localStorage.getItem('user')
+      const userPayloadString = JSON.stringify(user)
+      //IF PAYLOAD !==LOCAL
+      if (checkUserInLocal) {
+        if (checkUserInLocal !== userPayloadString) {
+          console.log('IF PAYLOAD !==LOCAL')
+          dispatch('POST_USER_DB', user) //NEED TO UPDATE DATA ON SERVER
+        } else {
+          Object.keys(state.user).length == 0 && commit('SET_USER', user)
+        }
+      } else {
+        //USER LOCAL IS NOT EXIST
+        try {
+          const { request: getUser, response } = useFetch<userType>(
+            `/api/user/${user.info.userId}?getRegisterUser=true`
+          )
+          await getUser()
+          //USER IN SERVER IS EXIST => COMMIT
+          const userServer = response.value
+          Object.keys(state.user).length == 0 && commit('SET_USER', userServer)
+        } catch (error) {
+          console.log(error, 'USER IS NOT EXIST IN DB')
+          //USER IS NOT EXIST, ADD NEW USER IN DB
+          dispatch('POST_USER_DB', user)
+        }
+      }
+    },
+    async POST_USER_DB({ commit }, user: userType) {
+      try {
+        const { request: postUser } = useFetch<string>(
+          `/api/user/${user.info.userId}?postRegisterUser=true`,
+          {
+            method: 'POST', // или 'PUT'
+            body: JSON.stringify(user),
+          }
+        )
+        await postUser()
+        commit('SET_USER', user)
+      } catch (error) {
+        console.log(error, 'SAVE USER IN A SERVER IS FAILED')
+      }
+    },
+
+
     // async GET_auth({ commit, state }) {
     //   let clientPrincipal = null
     //   let responseUserAuth
